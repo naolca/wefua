315af38c02fbda8ba446cbe49569b83cbc114e92
naolca <firaolibrahim28@gmail.com>
2023-10-23 22:51:59 +0300

    add the necessary firebase config

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
+const admin = require('firebase-admin');
 const express = require('express'); 
 
 const app = express(); 
 const PORT = 3000; 
+
+const serviceAccount = require('/home/firaol/Desktop/wefua/wefua/backend/wefua-5a385-firebase-adminsdk-ahbcm-bf9e75e3b7.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://wefua-5a385.firebaseio.com',
+});
+
+
 
 app.get('/', (req, res) => {
     res.send("Hello World");
